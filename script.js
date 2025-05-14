4fb74b4b4cd7f86c492f07f42f85a4d5caa8a899
bryanT062 <bryantate899@gmail.com>
2025-05-14 16:32:02 +0200

    disposition des cards

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+const sectionElement = document.querySelector(".contener");
 
-let donator = [""];
+fetch("https://randomuser.me/api/?results=50")
+  .then((response) => {
+    return response.json()
+})
+  
+  .then((json) => {
+    console.log(json);
+    const resultsArray = json.results;
 
-
-fetch("https://randomuser.me/api/?results=50").then (response => {
- console.log(response);
-
-});
+    for (const user of resultsArray) {
+        const fullName = user.name.first  +  user.name.last;
+        const country = user.country + user.state;
+        const picture = user.picture.medium;
+        const phone = user.cell;
+      const divElement = document.createElement("div");
+      sectionElement.appendChild(divElement);
+      divElement.classList.toggle("active");
+      divElement.innerHTML =`
+          <img src="${picture}">
+         <p><strong>${fullName}</strong> </p>
+         <p><strong>${country}</strong> </p>
+         <p><strong>${phone}</strong> </p>
+         
+         `
+    
+    }
+  });
